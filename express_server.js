c9ca0915017b17c118660fbc79b4d5bb442029d6
Gabi Geollegue <gabigeollegue@gmail.com>
2019-10-01 22:38:58 +0000

    added log in and log out functionality with cookies

--- express_server.js
+++ express_server.js
@@ -10,9 +10,10 @@ const generateRandomString = () => {
   }
   return result;
 };
+const cookieparser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieparser());
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -21,12 +22,17 @@ const urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/:shortURL", (req, res) => {
@@ -37,10 +43,17 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
 
 //add new URLs
@@ -60,6 +73,13 @@ app.post("/urls/:shortURL/delete",(req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+//login cookies
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie('username', username);
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
