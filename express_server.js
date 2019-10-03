2f55a0269f0aa87969edb10cfe13e4eaedd6387c
Gabi Geollegue <gabigeollegue@gmail.com>
2019-10-03 19:44:41 +0000

    added users obj and urlDatabase obj to db.js

--- express_server.js
+++ express_server.js
@@ -6,24 +6,7 @@ const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const { generateRandomString, checkEmail, getUserByEmail, urlsForUser } = require('./helpers');
-
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
-
-const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
-  "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" }
-};
+const {users, urlDatabase} = require("./db.js");
 
 app.set("view engine", "ejs");
 
