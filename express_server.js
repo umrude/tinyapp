331347bc5153c4cc364bd9133e479edfa0aa4718
Gabi Geollegue <gabigeollegue@gmail.com>
2019-10-03 20:05:51 +0000

    added morgan and init README.md, bit of refactoring for readability

--- express_server.js
+++ express_server.js
@@ -5,19 +5,21 @@ const PORT = 3000;
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 const routes = require("./routes/routes.js");
+const morgan = require('morgan');
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(morgan('combined'));
 app.use(cookieSession({
   name: 'session',
   keys: ['secret_keys'],
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
+//uses routes.js instead of crowding the express_server.js
+app.use("/", routes);
 
 //gets the server to listen for input
 app.listen(PORT, () => {
   console.log(`Welcome to TinyApp on PORT: ${PORT}!`);
 });
 
-app.use("/", routes);
